57debad7dbb0a4fdd3956fe0ebaaf6ef7c0e0b9f
mitumial <ace.vortex76@gmail.com>
2025-09-18 22:52:35 -0500

    fix layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      {children}
+    <div className="flex h-screen w-screen">
+      <div className="flex-1">
+        <header className="flex p-10">
+          <div className="w-[147px] h-[23px] flex items-center gap-4">
+            <img className="h-4 w-4" src=" /img/logo.png" alt="" />
+            <h1 className="text-zinc-800 font-bold text-[18px] leading-[23px] tracking-[0px]">VivaMedellín</h1>
+          </div>
+        </header>
+        {children}
+      </div>
+      <div className="flex-1">
+        <img className="h-full w-full object-cover " src="/img/udea.jpg" alt="" />
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,6 @@ export default function RootLayout({
     <html lang="es">
       <body className={inter.className}
       >
-        <header className="flex p-10">
-          <div className="w-[147px] h-[23px] flex items-center gap-4">
-            <img className="h-4 w-4" src=" /img/logo.png" alt="" />
-            <h1 className="font-inter text-zinc-800 font-bold text-[18px] leading-[23px] tracking-[0px]">VivaMedellín</h1>
-          </div>
-        </header>
         <div className="">{children}</div>
 
       </body>
