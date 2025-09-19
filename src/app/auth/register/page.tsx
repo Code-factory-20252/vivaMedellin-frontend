5f38677e3eaee33defaaa0623f99d378e1f7cff8
mitumial <ace.vortex76@gmail.com>
2025-09-18 23:48:29 -0500

    remove unused imports

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,6 +1,3 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
 import InputFormat from "@/app/components/Input";
 
 export default function Page() {
