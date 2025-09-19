182e8a8c6a66dd7e576fd6864f9374229d1e3a69
mitumial <ace.vortex76@gmail.com>
2025-09-18 23:32:19 -0500

    add id to label in ragister

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
     <h2 className="text-slate-700 font-semibold text-3xl leading-9  text-center">Crea tu cuenta</h2>
     <p className="text-slate-400 font-semibold text-sm leading-5 tracking-[0%] text-center">Únete a la comunidad de eventos más vibrante de Medellín.</p>
     <div className="flex flex-col gap-1.5 w-md">
-      <Label htmlFor="" className="text-slate-900 font-medium leading-5" >Correo electrónico</Label>
-      <Input className="border-slate-300 placeholder:leading-6 placeholder:text-slate-400" type="email" placeholder="Ingrese el correo electrónico" />
+      <Label htmlFor="email" className="text-slate-900 font-medium leading-5" >Correo electrónico</Label>
+      <Input id="email" className="border-slate-300 placeholder:leading-6 placeholder:text-slate-400" type="email" placeholder="Ingrese el correo electrónico" />
     </div>
   </div>;
 }
