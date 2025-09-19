67f06b6da73d09402e725befcb98cc5438c48a93
mitumial <ace.vortex76@gmail.com>
2025-09-18 23:54:57 -0500

    add link to register page from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+
 export default function Home() {
   return (<>
     <h1 className="text-6xl">Dashboard en Desarrollo</h1>
+    <Link href="/auth/register">Registrarse</Link>
   </>);
 }
