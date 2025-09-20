92a19efb80a7a18d8513b9c83ca64f3a040e6d49
mitumial <ace.vortex76@gmail.com>
2025-09-20 17:53:45 -0500

    extract register form into its own file

---
+++ src/app/auth/register/register-form.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form"
+
+import FormInput from "@/app/components/CustomInput";
+import { FormLabel } from "@/components/ui/form";
+
+const registerSchema = z.object({
+  email: z.string().min(1, "El correo electrónico es obligatorio"),
+});
+
+export default function RegisterForm() {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: ""
+    }
+  });
+
+
+  return <Form {...form}>
+    <FormField
+      name="email"
+      render={() => (
+        <FormItem>
+          <FormLabel htmlFor="email">Correo electrónico</FormLabel>
+          <FormControl>
+            <FormInput type="email" placeholder="Ingrese el correo electrónico" />
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  </Form>
+}
