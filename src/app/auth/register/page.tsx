2b84b658f6c4fa911409ab44746bd2f3cf2b4505
mitumial <ace.vortex76@gmail.com>
2025-09-20 17:57:32 -0500

    add layout styling for register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -4,7 +4,8 @@ export default function Page() {
   return <div className="flex flex-col items-center gap-5">
     <h2 className="text-slate-700 font-semibold text-3xl leading-9  text-center">Crea tu cuenta</h2>
     <p className="text-slate-400 font-semibold text-sm leading-5 tracking-[0%] text-center">Únete a la comunidad de eventos más vibrante de Medellín.</p>
-    <RegisterForm />
+    <div className="flex flex-col gap-1.5 w-md">
+      <RegisterForm />
+    </div>
   </div>
-
 }
