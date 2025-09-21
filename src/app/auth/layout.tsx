d166b3f0c569a36109eed3891ed788e5c1f6d215
mitumial <ace.vortex76@gmail.com>
2025-09-20 22:43:49 -0500

    align layout for register page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen w-screen">
-      <div className="flex-1">
-        <header className="flex p-10">
-          <div className="w-[147px] h-[23px] flex items-center gap-4">
+      <div className="flex-1 relative">
+        <header className="absolute mt-0 flex p-10">
+          <div className="w-36 h-6 flex items-center gap-4">
             <Image width={16} height={16} src="/img/logo.png" alt="" />
             <h1 className="text-zinc-800 font-bold text-[18px] leading-[23px] tracking-[0px]">VivaMedellín</h1>
           </div>
