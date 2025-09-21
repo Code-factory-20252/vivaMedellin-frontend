b1cefb92a9d2f98642530d6bd8924de04a8244f9
mitumial <ace.vortex76@gmail.com>
2025-09-20 21:25:38 -0500

    clean up input styling

--- src/app/components/CustomInput.tsx
+++ src/app/components/CustomInput.tsx
@@ -3,6 +3,6 @@ import { Input } from "@/components/ui/input"
 export default function CustomInput(props: React.ComponentProps<typeof Input>) {
   return (
     <Input {...props}
-      className="border-slate-300 placeholder:leading-6 placeholder:text-slate-400" />
+      className="h-10 pt-2 pb-2 border-slate-300 placeholder:text-base placeholder:leading-5  placeholder:text-slate-400" />
   )
 }
