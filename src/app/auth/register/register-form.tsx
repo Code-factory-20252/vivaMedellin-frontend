3939d3a96b0e28ae0c193c507ad752dd623f56e3
mitumial <ace.vortex76@gmail.com>
2025-09-20 20:17:59 -0500

    add all fields to registerForm schema

--- src/app/auth/register/register-form.tsx
+++ src/app/auth/register/register-form.tsx
@@ -13,14 +13,20 @@ import FormInput from "@/app/components/CustomInput";
 import { FormLabel } from "@/components/ui/form";
 
 const registerSchema = z.object({
+  username: z.string().min(1, "El nombre de usuario es obligatorio"),
   email: z.string().min(1, "El correo electrónico es obligatorio"),
+  password: z.string(),
+  confirmPassword: z.string(),
 });
 
 export default function RegisterForm() {
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
     defaultValues: {
-      email: ""
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     }
   });
 
