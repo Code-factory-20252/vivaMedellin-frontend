6e1921cf51446b0294308e4a38e1cda546233405
mitumial <ace.vortex76@gmail.com>
2025-09-21 19:50:37 -0500

    fix name for custom button

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 
-export default function CustomInput(props: React.ComponentProps<typeof Button>) {
+export default function CustomButton(props: React.ComponentProps<typeof Button>) {
   return (
     <Button {...props}
       className="bg-sky-500 rounded-full h-14 text-lg font-semibold  hover:bg-sky-600" />
