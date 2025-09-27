28a0fc2d4f7605e781892afe170fb2f073b2f1d5
mitumial <ace.vortex76@gmail.com>
2025-09-27 15:54:52 -0500

    restore correct login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,33 +1,17 @@
-import InputFormat from "@/components/ui/CustomInput";
+import LoginForm from "../../../components/ui/LoginForm";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center gap-5 min-h-screen justify-center">
+    <main className="flex flex-col items-center justify-center min-h-screen gap-5">
       <img
         src="/img/logo.png"
-        alt="Logo VIVE Medellín"
+        alt="Logo"
         className="h-14 mb-4"
       />
       <h2 className="text-slate-700 font-semibold text-2xl leading-9 text-center">
         Inicio de sesión
       </h2>
-      <div className="flex flex-col gap-1">
-        <label className="text-sm text-gray-600">Correo electrónico*</label>
-        <InputFormat />
-      </div>
-      <div className="flex flex-col gap-1">
-        <label className="text-sm text-gray-600">Contraseña*</label>
-        <InputFormat />
-      </div>
-      <a
-        href="#"
-        className="text-sm text-blue-500 hover:underline self-end pr-4"
-      >
-        ¿Olvidaste tu contraseña?
-      </a>
-      <button className="w-full max-w-sm py-2 bg-gray-400 text-white rounded-md hover:bg-gray-500 transition">
-        Iniciar sesión
-      </button>
-    </div>
+      <LoginForm />
+    </main>
   );
 }
